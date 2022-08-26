0b169fa1fd7b924eeb05fe639f14b6d2a08539fd
soderque <76970439+soderque@users.noreply.github.com>
2022-08-26 13:27:45 -0700

    Initial commit

---
+++ MTH/Matplotlib.py
@@ -0,0 +1,142 @@
+# matplotlib_intro.py
+#importing NumPy and matplotlib.
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Problem 1
+def var_of_means(n):
+    """Construct a random matrix A with values drawn from the standard normal
+    distribution. Calculate the mean value of each row, then calculate the
+    variance of these means. Return the variance.
+    Parameters:
+        n (int): The number of rows and columns in the matrix A.
+    Returns:
+        (float) The variance of the means of each row.
+    """
+    varray = np.random.normal(size=(n,n))
+    meanrandom = varray.mean(axis=0)  #compute the mean of each row.
+    return np.var(meanrandom)
+
+def prob1():
+    """Create an array of the results of var_of_means() with inputs
+    n = 100, 200, ..., 1000. Plot and show the resulting array.
+    """
+    y=np.zeros(10)
+    for n in range(100, 1100, 100):
+        for i in range(0, 10, 1):
+            y[i]=np.array(var_of_means(n))
+    
+    plt.plot(y)
+    plt.show()
+    return 0
+print(prob1())
+
+
+# Problem 2
+def prob2():
+    """Plot the functions sin(x), cos(x), and arctan(x) on the domain
+    [-2pi, 2pi]. Make sure the domain is refined enough to produce a figure
+    with good resolution.
+    """
+    x = np.linspace(-2*np.pi, 2*np.pi, 100)
+    y1=np.sin(x)
+    y2=np.cos(x)
+    y3=np.arctan(x)
+    
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.plot(x, y3)
+    plt.show()
+    return 0
+
+# Problem 3
+def prob3():
+    """Plot the curve f(x) = 1/(x-1) on the domain [-2,6].
+        1. Split the domain so that the curve looks discontinuous.
+        2. Plot both curves with a thick, dashed magenta line.
+        3. Set the range of the x-axis to [-2,6] and the range of the
+           y-axis to [-6,6].
+    """
+    x1 = np.linspace(-2, 1, 50, endpoint=False)
+    x2 = np.linspace(1, 6, 50, endpoint=False)
+    y1 = np.true_divide(1,x1-1)
+    y2 = np.true_divide(1,x2-1)
+    plt.plot(x1, y1, 'm--', lw=4)
+    plt.plot(x2, y2, 'm--', lw=4)
+    plt.xlim(-2,6)
+    plt.ylim(-6,6)
+    plt.show()
+    return 0
+print(prob3())
+
+# Problem 4
+def prob4():
+    """Plot the functions sin(x), sin(2x), 2sin(x), and 2sin(2x) on the
+    domain [0, 2pi].
+        1. Arrange the plots in a square grid of four subplots.
+        2. Set the limits of each subplot to [0, 2pi]x[-2, 2].
+        3. Give each subplot an appropriate title.
+        4. Give the overall figure a title.
+        5. Use the following line colors and styles.
+              sin(x): green solid line.
+             sin(2x): red dashed line.
+             2sin(x): blue dashed line.
+            2sin(2x): magenta dotted line.
+    """
+    x = np.linspace(0, 2*np.pi, 100)
+    ax1 = plt.subplot(221)
+    ax1.set_title("The normal sine function")
+    ax1.plot(x, np.sin(x), 'g')
+    ax2 = plt.subplot(222)
+    ax2.set_title("Double frequency")
+    ax2.plot(x, np.sin(2*x), 'r--')
+    ax3 = plt.subplot(223)
+    ax3.set_title("Amplitute 2")
+    ax3.plot(x, 2*np.sin(x), 'b--')
+    ax4 = plt.subplot(224)
+    ax4.set_title("Amplitutde 2, Double frequency")
+    ax4.plot(x, 2*np.sin(2*x), 'm:')
+    plt.axis([0, 2*np.pi, -2, 2])
+    plt.suptitle("A family of sine functions")
+    plt.show()
+    return 0
+
+
+# Problem 5
+def prob5():
+    """Visualize the data in FARS.npy. Use np.load() to load the data, then
+    create a single figure with two subplots:
+        1. A scatter plot of longitudes against latitudes. Because of the
+            large number of data points, use black pixel markers (use "k,"
+            as the third argument to plt.plot()). Label both axes.
+        2. A histogram of the hours of the day, with one bin per hour.
+            Label and set the limits of the x-axis.
+    """
+    raise NotImplementedError("Problem 5 Incomplete")
+
+
+# Problem 6
+def prob6():
+    """Plot the function f(x,y) = sin(x)sin(y)/xy on the domain
+    [-2pi, 2pi]x[-2pi, 2pi].
+        1. Create 2 subplots: one with a heat map of f, and one with a contour
+            map of f. Choose an appropriate number of level curves, or specify
+            the curves yourself.
+        2. Set the limits of each subplot to [-2pi, 2pi]x[-2pi, 2pi].
+        3. Choose a non-default color scheme.
+        4. Add a colorbar to each subplot.
+    """
+    x = np.linspace(-2*np.pi, 2*np.pi, 50)
+    y = x.copy()
+    X, Y = np.meshgrid(x, y)
+    g = np.true_divide(np.sin(X)*np.sin(Y), X*Y)
+    plt.subplot(121)
+    plt.pcolormesh(X, Y, g, cmap="gist_earth")
+    plt.colorbar()
+    plt.subplot(122)
+    plt.contour(X, Y, g, 15, cmap="rainbow")
+    plt.colorbar()
+    plt.axis([-2*np.pi, 2*np.pi, -2*np.pi, 2*np.pi])
+    plt.show()
+    return 0
+print(prob6())
